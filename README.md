3ed8b1dd69d760544b4e5dd540439e27b159e816
MordantArc <116419257+MordantArc@users.noreply.github.com>
2024-10-06 16:04:39 -0400

    Comments on README.md
    Shortened the .py file by putting the comments on the README.md

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
-# WMU_aiaa_points_evaluation
-Evaluates the possible points we can score in the 24-25 competition.
+# WMU AIAA Points Evaluation
+Evaluates the possible points we can score in the 24-25 competition. Thats it
+
+# Formulas
+
+mission two has this formula:
+```M2 = 1 + [N_(fuel weight / time) / Max_(fuel weight / time] , where Max_(fuel weight / time) is the highest (fuel weight / time) score of all teams. ```
+
+mission three:
+```M3 = 2 + [N_(# laps flown + (bonus box score / X-1 test vehicle weight)) / Max_(# laps flown + (bonus box score / X1 test vehicle weight)] , where Max_(# laps flown + (bonus box score / X-1 test vehicle weight) is the highest (# laps flown + (bonus box score / X-1 test vehicle weight) score of all teams.```
+
+
+# Max Values
+Mission 1 is the easiest, we either get a point or we dont. We're assuming that if we cannot complete a mission, all subsequent missions are also a failure.
+
+For 2 and 3, each maximum value can be determined(within reason) letting us graph our data in multiple sets assuming the worst(as in other teams having the best score possible) and best (we have the highest score, which equates to a score of 2 for m2 and 3 for m3)
+
+
+_**For M2:**_
+The denominator is just the highest value of (fuel/time). The maximum weight of an aircraft is 55 lbs. This means the theoretical maximum would be closer to 45-50 lbs for fuel as the aircraft will take around 5-10 lbs (minimum)
+The longest time is about 5 minutes, but ideally we have the shortest time to complete 3 laps. We'll call it a minute thirty as a theoretical fastest model. We can work this back later as heavier aircraft may go slower.
+
+This means (with a very generous set of scores) that a max value can start around 1.5. Keep in mind that for the best team with this score, this only nets them the max 1 point (team_fuelweight/max_fuelweight). It just makes everyone elses score lower
+
+_**For M3:**_
+This one is more complicated, but I'll skim the explanation and give my estimates.
+8 laps max 
+best boxscore of 2.5
+best weight of 25g, 0.055 lbs
+
+this gives us a max score of (8+2.5/0.055) or 53.4545455. Again, this is not the actual score but the denominator. Max points on the flight portion is 3 (2 completion, 1 bonus points)
+
--- points calculation.py
+++ points calculation.py
@@ -14,14 +14,6 @@ ALL CALCULATIONS ARE IN LBS AND sec
 3 missions, ground ops, and report scores
 
 mission one is just completion for 1 point
-
-mission two has this formula:
-M2 = 1 + [N_(fuel weight / time) / Max_(fuel weight / time] , where Max_(fuel weight / time) is the highest (fuel weight / time) score of all teams. 
-
-mission three:
-M3 = 2 + [N_(# laps flown + (bonus box score / X-1 test vehicle weight)) / Max_(# laps flown + (bonus box score / X1 test vehicle weight)] , where Max_(# laps flown + (bonus box score / X-1 test vehicle weight) is the highest (# laps flown + (bonus box score / X-1 test vehicle weight) score of all teams.
-
-
 """
 
 def points_mission2(team_fuel, team_time, max_fueltime): # i know it doesnt have to be a function but it just makes me happier
@@ -55,35 +47,6 @@ def all_mission_calc(fuel_weight,highest_mission_achieved,m2_time=2.5,max_m2=0.5
 
 """
 This is where we start analyzing the possibilities using iteration.
-
-Mission 1 is the easiest, we either get a point or we dont. We're assuming that if we cannot complete a mission, all subsequent missions are also a failure.
-
-For 2 and 3, each maximum value can be determined(within reason) letting us graph our data in multiple sets assuming the worst(as in other teams having the best score possible) and best (we have the highest score, which equates to a score of 2 for m2 and 3 for m3)
-
-
-
-For M2
----
-The denominator is just the highest value of (fuel/time). The maximum weight of an aircraft is 55 lbs. This means the theoretical maximum would be closer to 45-50 lbs for fuel as the aircraft will take around 5-10 lbs (minimum)
-The longest time is about 5 minutes, but ideally we have the shortest time to complete 3 laps. We'll call it a minute thirty as a theoretical fastest model. We can work this back later as heavier aircraft may go slower.
-
-This means (with a very generous set of scores) that a max value can start around 1.5. Keep in mind that for the best team with this score, this only nets them the max 1 point (team_fuelweight/max_fuelweight). It just makes everyone elses score lower
-
-
-
-For M3
----
-Theis one is more complicated, but I'll skim the explanation and give my estimates.
-8 laps max 
-best boxscore of 2.5
-best weight of 25g, 0.055 lbs
-
-this gives us a max score of (8+2.5/0.055) or 53.4545455. Again, this is not the actual score but the denominator. Max points on the flight portion is 3 (2 completion, 1 bonus points)
-
-
-
-
-
 
 Given all this cope, the first function will just handle writing this data to a csv (or xlsx if I'm feeling out there) so I can log data in an iteration format.
 We are also assuming we complete all 3 missions (we arent going to win if we dont)
